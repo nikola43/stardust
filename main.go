3832e5e25aafdb79ab7496cbd0ae62271ea7a42e
paulo <pauloxti@gmail.com>
2022-07-22 13:17:28 +0200

    encrypt file

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"crypto/rand"
 	"flag"
 	"fmt"
 	"log"
@@ -9,6 +10,7 @@ import (
 	"os/signal"
 
 	"github.com/nikola43/stardust/config"
+	crypto "github.com/nikola43/stardust/crypto"
 	"github.com/nikola43/stardust/router"
 	"github.com/nikola43/stardust/server"
 	sysinfo "github.com/nikola43/stardust/sysinfo"
@@ -29,12 +31,21 @@ func init() {
 }
 
 func main() {
+
+	sysHash := GetSysInfo()
+
+	key := make([]byte, 32)
+	rand.Read(key)
+	fmt.Println(key)
+	crypto.EncryptSysData([]byte(sysHash), []byte(key))
+	crypto.DecryptFile("sysdata.txt.bin", []byte(key))
+	os.Exit(0)
+
 	// create unix syscall
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, os.Interrupt, os.Kill)
 	notify := make(chan struct{}, 1)
 
-	GetSysInfo()
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	UpdateEtcdConf()
@@ -97,10 +108,11 @@ func GetEtcdConfig() *config.Config {
 	return cfg
 }
 
-func GetSysInfo() {
+func GetSysInfo() string {
 
 	info := sysinfo.NewSysInfo()
 	fmt.Printf("%+v\n", info)
 	fmt.Printf("%+s\n", info.ToString())
 	fmt.Printf("%+s\n", info.ToHash())
+	return info.ToHash()
 }
