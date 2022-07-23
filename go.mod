e8d5b75397bc62994d5cdf6a3899c3616428a7bd
paulo <pauloxti@gmail.com>
2022-07-23 15:25:25 +0200

    web3 added

--- go.mod
+++ go.mod
@@ -11,12 +11,29 @@ require (
 )
 
 require (
+	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
 	github.com/coreos/go-semver v0.3.0 // indirect
 	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
+	github.com/deckarep/golang-set v1.8.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/ethereum/go-ethereum v1.10.19 // indirect
+	github.com/fatih/color v1.13.0 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/hokaccha/go-prettyjson v0.0.0-20211117102719-0474bc63780f // indirect
+	github.com/hrharder/go-gas v1.0.1 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mdp/qrterminal v1.0.1 // indirect
+	github.com/nikola43/web3golanghelper v0.0.0-20220704230832-2ae81b379449 // indirect
+	github.com/rjeczalik/notify v0.9.2 // indirect
 	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
+	github.com/shopspring/decimal v1.3.1 // indirect
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
 	github.com/tklauser/go-sysconf v0.3.10 // indirect
 	github.com/tklauser/numcpus v0.5.0 // indirect
 	github.com/vishvananda/netns v0.0.0-20211101163701-50045581ed74 // indirect
@@ -26,9 +43,12 @@ require (
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
 	go.uber.org/zap v1.21.0 // indirect
+	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
 	golang.org/x/net v0.0.0-20220708220712-1185a9018129 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20220720214146-176da50484ac // indirect
 	google.golang.org/grpc v1.48.0 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
+	rsc.io/qr v0.2.0 // indirect
 )
--- main.go
+++ main.go
@@ -8,12 +8,20 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/fatih/color"
 	"github.com/nikola43/stardust/config"
 	crypto "github.com/nikola43/stardust/crypto"
 	"github.com/nikola43/stardust/router"
 	"github.com/nikola43/stardust/server"
 	sysinfo "github.com/nikola43/stardust/sysinfo"
+	"github.com/nikola43/web3golanghelper/web3helper"
+	"github.com/nikola43/stardust/NodeManagerV83"
 )
 
 var (
@@ -115,4 +123,60 @@ func GetSysInfo() string {
 	fmt.Printf("%+s\n", info.ToString())
 	fmt.Printf("%+s\n", info.ToHash())
 	return info.ToHash()
+}
+
+func InitWeb3() {
+	pk := "b366406bc0b4883b9b4b3b41117d6c62839174b7d21ec32a5ad0cc76cb3496bd"
+	rpcUrl := "https://speedy-nodes-nyc.moralis.io/84a2745d907034e6d388f8d6/avalanche/testnet"
+	wsUrl := "wss://speedy-nodes-nyc.moralis.io/84a2745d907034e6d388f8d6/avalanche/testnet/ws"
+	web3GolangHelper := web3helper.NewWeb3GolangHelper(rpcUrl, wsUrl, pk)
+
+	chainID, err := web3GolangHelper.HttpClient().NetworkID(context.Background())
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Chain Id: " + chainID.String())
+
+	proccessEvents(web3GolangHelper)
+}
+
+func proccessEvents(web3GolangHelper *web3helper.Web3GolangHelper) {
+	nodeAddress := "0x2Fcd73952e53aAd026c378F378812E5bb069eF6E"
+	nodeAbi, _ := abi.JSON(strings.NewReader(string(NodeManagerV83.NodeManagerV83ABI)))
+	fmt.Println(color.YellowString("  ----------------- Blockchain Events -----------------"))
+	fmt.Println(color.CyanString("\tListen node manager address: "), color.GreenString(nodeAddress))
+	logs := make(chan types.Log)
+	sub := BuildContractEventSubscription(web3GolangHelper, nodeAddress, logs)
+
+	for {
+		select {
+		case err := <-sub.Err():
+			fmt.Println(err)
+			//out <- err.Error()
+
+		case vLog := <-logs:
+			fmt.Println("paco")
+			fmt.Println("vLog.TxHash: " + vLog.TxHash.Hex())
+			res, err := nodeAbi.Unpack("GiftCardPayed", vLog.Data)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(res)
+			//services.SetGiftCardIntentPayment(res[2].(string))
+
+		}
+	}
+}
+
+func BuildContractEventSubscription(web3GolangHelper *web3helper.Web3GolangHelper, contractAddress string, logs chan types.Log) ethereum.Subscription {
+
+	query := ethereum.FilterQuery{
+		Addresses: []common.Address{common.HexToAddress(contractAddress)},
+	}
+
+	sub, err := web3GolangHelper.WebSocketClient().SubscribeFilterLogs(context.Background(), query, logs)
+	if err != nil {
+		fmt.Println(sub)
+	}
+	return sub
 }
