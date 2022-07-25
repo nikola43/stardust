be06666798b9c513e67afab200026b095036557f
paulo <pauloxti@gmail.com>
2022-07-25 21:17:14 +0200

    cli

--- cli/upload_command.go
+++ cli/upload_command.go
@@ -3,6 +3,9 @@ package cli
 import (
 	"context"
 	"fmt"
+	"net"
+
+	"github.com/nikola43/stardust/crypto"
 )
 
 type UploadCommand struct {
@@ -22,21 +25,39 @@ func newUploadCommand() Command {
 	}
 }
 
-func (c *UploadCommand) uploadMasterWallet() error {
-	fmt.Println("upload master wallet")
-	return nil
-}
-
-func (c *UploadCommand) uploadBTCWallet() error {
-	fmt.Println("upload BTC wallet")
-	return nil
-}
-
 func (c *UploadCommand) ExecCommand(ctx context.Context, args []string) error {
 	if len(args) == 0 {
 		return ErrorFromString(fmt.Sprintf("%s: no subcommand passed", uploadCommand))
 	}
 	c.args = &Args{args}
 
-	return ErrorFromString(fmt.Sprintf("%s: invalid subcommand passed", uploadCommand))
+	msg := "hello dani"
+
+	sCipher := &crypto.GCM{
+		Passphrase: "6368616e676520746869732070617373776f726420746f206120736563726574",
+	}
+	sCipher.Init()
+
+	b, err := sCipher.Encrypt([]byte(msg))
+	if err != nil {
+		fmt.Println("Encrypt")
+		panic(err)
+	}
+
+	conn, err := net.Dial("udp", "146.190.239.223:8085")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if conn == nil {
+
+	}
+	writtedBytes, writeUdpErr := fmt.Fprintf(conn, string(b))
+	if writeUdpErr != nil {
+		fmt.Println("Encrypt")
+		panic(writeUdpErr)
+	}
+	fmt.Println(writtedBytes)
+	//return ErrorFromString(fmt.Sprintf("%s: invalid subcommand passed", uploadCommand))
+	return nil
 }
